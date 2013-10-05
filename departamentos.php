219f1482ba3ca28b8c6a85dea2fd5868aad73e76
Gerardo <g3rcar@gmail.com>
2013-10-05 17:24:42 -0600

    Cambios en titulos de los mantenimientls

--- departamentos.php
+++ departamentos.php
@@ -188,7 +188,7 @@ include("res/partes/encabezado.php");
 		
 		<div class="modal-header">
 			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-			<h3 id="modalHead">Agregar departamento</h3>
+			<h3 id="modalHead">Departamento</h3>
 		</div>
 		<div class="modal-body">
 			<form>
--- municipios.php
+++ municipios.php
@@ -229,7 +229,7 @@ include("res/partes/encabezado.php");
 		
 		<div class="modal-header">
 			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-			<h3 id="modalHead">Agregar municipio</h3>
+			<h3 id="modalHead">Municipio</h3>
 		</div>
 		<div class="modal-body">
 			<form>
